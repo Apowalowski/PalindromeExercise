ae0fdb166f68c96a79e28c7c40e589b64f25aef9
Apowalowski <acpowalowski@gmail.com>
2021-04-26 14:59:39 -0400

    PalindromeExercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string myString )
+        {
+            return myString.SequenceEqual(myString.Reverse());
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("Low", false)]
+        public void IsAPalindrome(string myString, bool expected)
         {
-
+            //Arrange
+            var test = new WordSmith();
+            //Act
+            var actual = test.IsAPalindrome(myString);
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
